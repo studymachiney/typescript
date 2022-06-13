396bcd591e5503709382431ab3eef591105ad0d5
studymachine <1771134505@qq.com>
2022-06-13 10:15:17 +0800

    单词接龙、双向广度优先搜索

--- src/algorithm/BreadthFirstSearch/127.ladderLength.ts
+++ src/algorithm/BreadthFirstSearch/127.ladderLength.ts
@@ -33,4 +33,53 @@
         wordList = ['hot', 'dot', 'dog', 'lot', 'log', 'cog']
     console.log(ladderLength(beginWord, endWord, wordList))
 })()
+;(() => {
+    function ladderLength(
+        beginWord: string,
+        endWord: string,
+        wordList: string[]
+    ): number {
+        const wordSet = new Set(wordList)
+        if (!wordSet.has(endWord)) {
+            return 0
+        }
+        let beginSet = new Set<string>(),
+            endSet = new Set<string>()
+        beginSet.add(beginWord)
+        endSet.add(endWord)
 
+        let n = 1
+        while (beginSet.size > 0) {
+            const nextBegins = new Set<string>()
+            if (beginSet.size > endSet.size) {
+                let q = beginSet
+                beginSet = endSet
+                endSet = q
+            }
+            for (const str of beginSet) {
+                for (let i = 0; i < str.length; i++) {
+                    for (
+                        let j = 'a'.charCodeAt(0);
+                        j <= 'z'.charCodeAt(0);
+                        j++
+                    ) {
+                        let newWord =
+                            str.slice(0, i) +
+                            String.fromCharCode(j) +
+                            str.slice(i + 1)
+                        if (endSet.has(newWord)) {
+                            return n + 1
+                        }
+                        if (wordSet.has(newWord)) {
+                            nextBegins.add(newWord)
+                            wordSet.delete(newWord)
+                        }
+                    }
+                }
+            }
+            beginSet = nextBegins
+            n++
+        }
+        return 0
+    }
+})()
