3fe9a218103e3d2ce55679a23e48b82f986f276e
studymachine <1771134505@qq.com>
2022-06-29 09:39:57 +0800

    最长公共子序列

---
+++ src/algorithm/dynamicProgramming/1143.longestCommonSubsequence.ts
@@ -0,0 +1,21 @@
+;(() => {
+    function longestCommonSubsequence(text1: string, text2: string): number {
+        const m = text1.length,
+            n = text2.length
+        const dp: number[][] = new Array(m + 1)
+            .fill(0)
+            .map(() => new Array(n + 1).fill(0))
+        for (let i = 1; i <= m; i++) {
+            const char1 = text1[i - 1]
+            for (let j = 1; j <= n; j++) {
+                const char2 = text2[j - 1]
+                if (char1 === char2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+                }
+            }
+        }
+        return dp[m][n]
+    }
+})()
